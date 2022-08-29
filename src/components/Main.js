d4c03224ba6f70164e560b0dd613939d22b485aa
catjeed <kadblp@yandex.ru>
2022-08-30 02:31:14 +0500

    remove extra React imports, add .idea into .gitignore

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import Card from "./Card";
 import '../index.css';
 import api from "../utilities/Api";
